4a74956df90d5f3ea91f3ec27dfb66f2eb29d94f
guilherme-ogreofcodes <guilhermeieoq@gmail.com>
2026-09-11 15:29:04 -0300

    Adicionar arquivos de projeto.

---
+++ MerceariaMVC/Data/MerceariaContext.cs
@@ -0,0 +1,15 @@
+﻿using MerceariaMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MerceariaMVC.Data
+{
+    public class MerceariaContext:DbContext
+    {
+        public MerceariaContext(DbContextOptions<MerceariaContext> options) : base(options) 
+        {
+        }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set;}
+    }
+}
+
